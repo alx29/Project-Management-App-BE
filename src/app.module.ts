414b1464abcbf9f70cfe0a3892c12f83c8db2182
alx29 <alex.paraschiv29@gmail.com>
2023-02-16 11:08:38 +0200

    connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+// mongodb+srv://alex:294318@cluster0.owg42th.mongodb.net/test
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://alex:294318@cluster0.owg42th.mongodb.net/test',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000).then(() => {
+    console.log('Succes')
+  }).catch((err) => {
+    console.log(err);
+  });
 }
 bootstrap();
