40e955eb47b369032b86ef56ee00b5dd1f29d222
alx29 <alex.paraschiv29@gmail.com>
2023-02-16 17:35:12 +0200

    added some configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { UserSchema } from './users/users.model';
 // mongodb+srv://alex:294318@cluster0.owg42th.mongodb.net/test
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://alex:294318@cluster0.owg42th.mongodb.net/test',
     ),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
