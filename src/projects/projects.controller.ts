287095f4b983f102aff29e15a3937375906c970b
alx29 <alex.paraschiv29@gmail.com>
2023-05-06 20:41:07 +0300

    protected some endpoints

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Param, Post, Get, Put } from '@nestjs/common';
+import { Body, Controller, Param, Post, Get, Put, UseGuards } from '@nestjs/common';
 import { ProjectDTO } from './projects.model';
 import { ProjectsService } from './projects.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('projects')
 export class ProjectsController {
-  constructor(private readonly projectsService: ProjectsService) { }
+  constructor(private readonly projectsService: ProjectsService) {}
 
+  @Roles('project_manager')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Post('/create_project')
-  async addProject(
-    @Body() body: ProjectDTO 
-  ) {
+  async addProject(@Body() body: ProjectDTO) {
     const project = await this.projectsService.insertProject(body);
-    
+
     return {
       msg: 'Project creates',
       projectName: project.name,
@@ -27,8 +30,13 @@ export class ProjectsController {
     return project;
   }
 
+  @Roles('project_manager')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Put(':id')
-  async updateProject(@Param('id') id: string, @Body() updateaProjectDTO: ProjectDTO) {
+  async updateProject(
+    @Param('id') id: string,
+    @Body() updateaProjectDTO: ProjectDTO,
+  ) {
     return this.projectsService.updateProject(id, updateaProjectDTO);
   }
 }
